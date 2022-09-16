7d9af4f124f5c95c0a1d6bc43bddbfb32619994f
robinvasquez <rvasquezb2@miumg.edu.gt>
2022-09-16 14:35:38 -0600

    Bugs fixed

--- src/main/java/com/ligas/api/ApiLigasApplication.java
+++ src/main/java/com/ligas/api/ApiLigasApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+@SpringBootApplication
 public class ApiLigasApplication {
 
 	public static void main(String[] args) {
@@ -18,7 +18,7 @@ public class ApiLigasApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
