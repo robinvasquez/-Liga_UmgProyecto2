9e05cbe1ac5474c64a48a52a61a98a3ece6fbda8
robinvasquez <rvasquezb2@miumg.edu.gt>
2022-09-16 12:24:19 -0600

    Exclusion desde application

--- src/main/java/com/ligas/api/ApiLigasApplication.java
+++ src/main/java/com/ligas/api/ApiLigasApplication.java
@@ -3,8 +3,9 @@ package com.ligas.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+ 
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class ApiLigasApplication {
 
 	public static void main(String[] args) {
