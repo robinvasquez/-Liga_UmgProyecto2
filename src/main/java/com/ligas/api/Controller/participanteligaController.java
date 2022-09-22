d440c68481c4cad39f68ca89dd53719330f8e3b0
robinvasquez <rvasquezb2@miumg.edu.gt>
2022-09-21 22:03:00 -0600

    metodos para consultar particiopantes

--- src/main/java/com/ligas/api/Controller/participanteligaController.java
+++ src/main/java/com/ligas/api/Controller/participanteligaController.java
@@ -1,4 +1,5 @@
 package com.ligas.api.Controller;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ligas.api.model.liga;
 import com.ligas.api.model.participanteliga;
 import com.ligas.api.model.solicitudesliga;
 import com.ligas.api.repository.participanteligaRepository;
@@ -27,6 +29,31 @@ public class participanteligaController {
 	public List<participanteliga> allPersons(){
 		return repositorys.findAll();
 	}
+	
+	@GetMapping("/participantesliga/{idLiga}")
+	public List<participanteliga> getParticipantes(@PathVariable Long idLiga) {
+		List<participanteliga> participantes =repositorys.findAll();
+		List<participanteliga> participantes2=new ArrayList<participanteliga>();
+		for(int i=0;i<participantes.size();i++) {
+			if(participantes.get(i).getId_Liga()==idLiga) {
+				participantes2.add(participantes.get(i));
+			}
+		}		
+		return participantes2;	
+	}
+	
+	@GetMapping("/ligasquepertenezco/{idUsuario}")
+	public List<participanteliga> getMisLigas(@PathVariable Long idUsuario) {
+		List<participanteliga> misligas =repositorys.findAll();
+		List<participanteliga> misligas2=new ArrayList<participanteliga>();
+		for(int i=0;i<misligas.size();i++) {
+			if(misligas.get(i).getId_Usuario()==idUsuario) {
+				misligas2.add(misligas.get(i));
+			}
+		}		
+		return misligas2;	
+	}
+	
 	@PostMapping("/participante")
 	public participanteliga createPerson(@RequestBody participanteliga participanteliga) {
 		return repositorys.save(participanteliga);
